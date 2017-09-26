5010ab69f9851e1f6d55dda1d288c5bb8f53f857
jEzEk <jezek@chicki.sk>
2017-09-26 17:51:13 +0200

    essential programs

--- install.sh
+++ install.sh
@@ -1,15 +1,29 @@
 #! /bin/bash
 
-SUDO=''
-if (( $EUID != 0 )); then
-    SUDO='sudo'
-fi
-
 # colors
 cDefault='\e[0m'
 cRed='\e[91m'
 cGreen='\e[92m'
+cBlue='\e[94m'
 cYellow='\e[93m'
+
+debug=0
+for arg in $@; do
+	case "$arg" in
+		debug ) debug=1;;
+		* )
+			echo -e $cRed"Unknown argument: $arg"$cDefault
+			exit 1
+			;;
+	esac
+done
+
+# sudo
+SUDO=''
+if (( $EUID != 0 )); then
+	SUDO='sudo'
+fi
+
 
 # Yes/no dialog. The first argument is the message that the user will see.
 # If the user enters n/N, send exit 1.
@@ -43,51 +57,94 @@ run(){
 	echo -e $cGreen$1$cDefault
 	$1
 	res=$?
-	echo -e $cYellow"Result: $res"$cDefault
+	if [ "$debug" = 1 ]; then
+		echo -e $cYellow"Result: $res"$cDefault
+	fi
 	return $res
 }
 
-# update & upgrade
+install(){
+	run $SUDO" apt install "$1
+	return $?
+}
+
+isCmd(){
+	type $1 >/dev/null 2>&1
+	return $?
+}
+
+### update & upgrade
 ##if check_yes_no "update & upgrade?" "n"; then
 ##	$SUDO apt update
 ##	$SUDO apt upgrade
 ##fi
 ##
 ##if check_yes_no "turn on sticky keys?" "n"; then
-##	if ! type xkbset 2>/dev/null; then
-##		echo "xkbset not installed"
-##		run "$SUDO apt install xkbset"
+##	xkbsetinstall=0
+##	if ! isCmd xkbset; then
+##		echo "need xkbset, which is not installed"
+##		if check_yes_no "install xkbset?"; then
+##			xkbsetinstall=1
+##			install "xkbset"
+##		fi
 ##	fi
-##	if type xkbset 2>/dev/null; then
+##	if isCmd xkbset; then
 ##		echo ""
 ##		run "xkbset a sticky -twokey -latchlock"
 ##		run "xkbset exp =sticky"
 ##		echo "you should add theese somewhere to startup, or turn on in settings..."
 ##		read
-##	else
+##	elif [ "$xkbsetinstall" = 1 ]; then
 ##		echo -e $cRed"xkbset install failed!"$cDefault
 ##	fi
 ##fi
+##
+##cd $HOME
 
-cd $HOME
 
-
-if ! type git 2>/dev/null; then
-	echo "need git and git not installed"
-	run "$SUDO apt install git"
-	if ! type git 2>/dev/null; then
-		echo -e $cRed"git install failed!"$cDefault
-		exit 1
+essentials=(git curl)
+missing=()
+for cmd in "${essentials[@]}"; do
+	echo "checking if $cmd is missing"
+	if ! isCmd $cmd; then
+		missing+=($cmd)
 	fi
+done
+echo "missing: ${missing[@]}"
+if [ ! ${#missing[@]} = 0 ]; then
+	echo "we need theese essential programs for this script:"
+	echo -e $cBlue${missing[@]}$cDefault
+	if check_yes_no "install and continue?"; then
+		local installed=()
+		for pkg in ${missing[@]}; do
+			install $pkg
+			if ! isCmd $pkg; then
+				echo -e $cRed"$pkg install failed!"$cDefault
+				#revert installed missing?
+				if [ ! ${#installed[@]} = 0 ]; then
+					echo "reverting installed missing: ${installed[@]}"
+					run $SUDO" apt remove ${installed[@]}"
+				fi
+				exit 255
+			fi
+			installed+=($pkg)
+		done
+	else #check_yes_no "install and continue?"
+		exit 1
+	fi #check_yes_no "install and continue?"
 fi
+unset missing
+unset essentials
+
 
 run "ssh -qT git@github.com"
-echo $?
-if [ "$?" = 1 ]; then
+gsa=$?
+if [ "$gsa" = 1 ]; then
 	echo "can connect to git via ssh"
 else
 	echo "can NOT connect to git via ssh"
 fi
+#TODO finish this
 exit
 
 DOTFILES=".dotfiles"
@@ -150,77 +207,77 @@ if [ -e $DOTPROFILE ]; then
 	fi
 fi
 
-if ! type vim 2>/dev/null; then
+if ! isCmd vim; then
 	echo "vim not installed"
 	if check_yes_no "install vim?"; then
-		run "$SUDO apt install vim"
+		install "vim"
 	fi
 fi
 
-if type vim 2>/dev/null; then
-if check_yes_no "configure vim?"; then
+if isCmd vim; then
+	if check_yes_no "configure vim?"; then
 
-VIMFILES="$DOTFILES/vim/files"
-if [ ! -d $VIMFILES ]; then
-	echo "directory $VIMFILES does not exists"
-	exit 1
-fi
+		VIMFILES="$DOTFILES/vim/files"
+		if [ ! -d $VIMFILES ]; then
+			echo "directory $VIMFILES does not exists"
+			exit 1
+		fi
 
-VIMDIR=".vim"
-if [ -d $VIMDIR ]; then
-	echo "$VIMDIR directory exists"
-	if check_yes_no "backup $VIMDIR?"; then
-		VIMBACKUP="$VIMDIR.bak"
-		echo "backing up to $VIMBACKUP"
-		run "mv $VIMDIR $VIMBACKUP"
-	else
-		run "rm -rf $VIMDIR"
-	fi
-	if [ -d $VIMDIR ]; then
-		echo "failed"
-		exit 1
-	fi
-fi
-run "cp -vilbr $VIMFILES/.vim ."
+		VIMDIR=".vim"
+		if [ -d $VIMDIR ]; then
+			echo "$VIMDIR directory exists"
+			if check_yes_no "backup $VIMDIR?"; then
+				VIMBACKUP="$VIMDIR.bak"
+				echo "backing up to $VIMBACKUP"
+				run "mv $VIMDIR $VIMBACKUP"
+			else
+				run "rm -rf $VIMDIR"
+			fi
+			if [ -d $VIMDIR ]; then
+				echo "failed"
+				exit 1
+			fi
+		fi
+		run "cp -vilbr $VIMFILES/.vim ."
 
-VIMRC=".vimrc"
-if [ ! -e "$VIMFILES/$VIMRC" ]; then
-    echo "file $VIMFILES/$VIMRC does not exists"
-	exit 1
-fi
+		VIMRC=".vimrc"
+		if [ ! -e "$VIMFILES/$VIMRC" ]; then
+			echo "file $VIMFILES/$VIMRC does not exists"
+			exit 1
+		fi
 
-if [ -e $VIMRC ]; then
-	echo "$VIMRC exists"
-	if check_yes_no "backup $VIMRC?"; then
-		VIMRCBACKUP="$VIMRC.bak"
-		echo "backing up to $VIMRCBACKUP"
-		run "mv $VIMRC $VIMRCBACKUP"
-	else
-		run "rm $VIMRC"
-	fi
-	if [ -e $VIMRC ]; then
-		echo "failed"
-		exit 1
-	fi
-fi
-run "cp -vilb $VIMFILES/$VIMRC ./$VIMRC"
+		if [ -e $VIMRC ]; then
+			echo "$VIMRC exists"
+			if check_yes_no "backup $VIMRC?"; then
+				VIMRCBACKUP="$VIMRC.bak"
+				echo "backing up to $VIMRCBACKUP"
+				run "mv $VIMRC $VIMRCBACKUP"
+			else
+				run "rm $VIMRC"
+			fi
+			if [ -e $VIMRC ]; then
+				echo "failed"
+				exit 1
+			fi
+		fi
+		run "cp -vilb $VIMFILES/$VIMRC ./$VIMRC"
 
-VIMPLUG="$VIMDIR/autoload/plug.vim"
-if [ -e $VIMPLUG ]; then
-	#TODO fonnt for airline
-	run "vim +PlugInstall +qall"
-else
-	echo "no $VIMPLUG"
-fi
+		VIMPLUG="$VIMDIR/autoload/plug.vim"
+		if [ -e $VIMPLUG ]; then
+			#TODO fonnt for airline
+			run "vim +PlugInstall +qall"
+		else
+			echo "no $VIMPLUG"
+		fi
 
-fi #check_yes_no "install an configure vim?"
+	fi #check_yes_no "install an configure vim?"
 fi #type vim 2>/dev/null
 
-if ! type gvim 2>/dev/null; then
+if ! isCmd gvim; then
 	echo "gvim not installed"
 	if check_yes_no "install vim-gtk3?"; then
-		run "$SUDO apt install vim-gtk3"
-		if ! type gvim 2>/dev/null; then
+		install "vim-gtk3"
+		if ! isCmd gvim; then
 			echo "failed"
 			exit 1
 		fi
@@ -228,19 +285,19 @@ if ! type gvim 2>/dev/null; then
 fi
 
 
-if ! type mc 2>/dev/null; then
+if ! isCmd mc; then
 	echo "mc not installed"
 	if check_yes_no "install mc?"; then
-		run "$SUDO apt install mc"
-		if ! type mc 2>/dev/null; then
+		install "mc"
+		if ! isCmd mc; then
 			echo "failed"
 			exit 1
 		fi
 	fi
 fi
 
-if type mc 2>/dev/null; then
-MCFILES="$DOTFILES/mc/files"
+if isCmd mc; then
+	MCFILES="$DOTFILES/mc/files"
 	if [ -d $MCFILES ]; then
 		if check_yes_no "configure mc from $MCFILES?"; then
 			run "cp -vibr $MCFILES/.config ."
@@ -249,20 +306,20 @@ MCFILES="$DOTFILES/mc/files"
 fi
 
 
-if ! type audacious 2>/dev/null; then
+if ! isCmd audacious; then
 	echo "audacious not found"
 	if check_yes_no "install audacious?"; then
-		run "$SUDO apt install audacious audacious-plugins"
-		if ! type audacious 2>/dev/null; then
+		install "audacious audacious-plugins"
+		if ! isCmd audacious; then
 			echo "failed"
 			exit 1
 		fi
-		if type rhythmbox 2>/dev/null; then
+		if isCmd rhythmbox; then
 			echo "rhythmbox found"
 			if check_yes_no "purge rhythmbox?"; then
 				run "$SUDO apt purge rhythmbox"
 			fi
-			if type rhythmbox 2>/dev/null; then
+			if isCmd rhythmbox; then
 				echo "purge failed, uninstall manualy"
 				read
 			fi
@@ -270,15 +327,15 @@ if ! type audacious 2>/dev/null; then
 	fi
 fi
 
-if type audacious 2>/dev/null; then
+if isCmd audacious; then
 	AUDACIOUSFILES="$DOTFILES/audacious/files"
 	if [ -d $AUDACIOUSFILES ]; then
 		if check_yes_no "configure audacious from $AUDACIOUSFILES?"; then
 			run "cp -vibr $AUDACIOUSFILES/.config ."
 		fi
-  	#TODO mimeapps.list text replace to .config/mimeapps.list
+		#TODO mimeapps.list text replace to .config/mimeapps.list
 		echo "associate audacious with audio files (from $AUDACIOUSFILES/mimeapps.list to .config/mimeapps.list"
-    read
+		read
 	fi
 fi
 
@@ -286,7 +343,7 @@ fi
 if ! type chromium-browser 2>/dev/null; then
 	echo "chromium not found"
 	if check_yes_no "install chromium?"; then
-		run "$SUDO apt install chromium-browser"
+		install "chromium-browser"
 		if ! type chromium-browser 2>/dev/null; then
 			echo "failed"
 			exit 1
@@ -294,12 +351,12 @@ if ! type chromium-browser 2>/dev/null; then
 	fi
 fi
 
-if type firefox 2>/dev/null; then
+if isCmd firefox; then
 	echo "firefox found"
 	if check_yes_no "purge firefox?"; then
 		run "$SUDO apt purge firefox"
 	fi
-	if type firefox 2>/dev/null; then
+	if isCmd firefox; then
 		echo "purge failed, uninstall manualy"
 		read
 	fi
