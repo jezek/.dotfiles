f97667d8fe803712478840d83802e76e6d9b8bb1
jEzEk <jezek@chicki.sk>
2017-09-29 21:27:04 +0200

    bash configuration

--- install.sh
+++ install.sh
@@ -1,18 +1,26 @@
 #! /bin/bash
 
 # colors
-cDefault='\e[0m'
-cRed='\e[91m' # errors
-cGreen='\e[92m' # commands
-cBlue='\e[94m' # packages
+cNone='\e[0m'
+# named colors
+cRed='\e[91m'
+cGreen='\e[92m'
 cYellow='\e[93m' # results
+cBlue='\e[94m'
+cMagenta='\e[95m'
+# layout colors
+cErr=$cRed
+cWarn=$cRed
+cCmd=$cGreen
+cPkg=$cBlue
+cFile=$cMagenta
 
 debug=0
 for arg in $@; do
 	case "$arg" in
 		debug ) debug=1;;
 		* )
-			echo -e $cRed"Unknown argument: $arg"$cDefault
+			echo -e $cErr"Unknown argument: $arg"$cNone
 			exit 1
 			;;
 	esac
@@ -48,7 +56,7 @@ check_yes_no(){
 				echo "No"
 				return 1;;
 			* )
-				echo -e "${cRed}Please answer y or n for yes or no.${cDefault}";;
+				echo -e "${cErr}Please answer y or n for yes or no.${cNone}";;
 		esac
 	done;
 	echo "Yes"
@@ -56,24 +64,24 @@ check_yes_no(){
 
 run(){
 	if [ ! $# = 1 ]; then
-		(>&2 echo -e $cRed"run: need 1 argument, got $#: "$cDefault$@)
+		(>&2 echo -e $cErr"run: need 1 argument, got $#: "$cNone$@)
 		return 255
 	fi
-	(>&2 echo -e $cGreen$1$cDefault)
+	(>&2 echo -e $cCmd$1$cNone)
 	eval $1
 	local res=$?
 	if [ "$debug" = 1 ]; then
 		local rc=$cGreen
 		if [ ! $res = 0 ]; then
-			local rc=$cRed
+			local rc=$cErr
 		fi
-		(>&2 echo -e "Result: "$rc$res$cDefault)
+		(>&2 echo -e "Result: "$rc$res$cNone)
 	fi
 	return $res
 }
 runRes(){
 	if [ ! $# = 2 ]; then
-		(>&2 echo -e $cRed"runRes: need 2 argument, got $#: "$cDefault$@)
+		(>&2 echo -e $cErr"runRes: need 2 argument, got $#: "$cNone$@)
 		return 255
 	fi
 	local resVar=$1
@@ -82,7 +90,7 @@ runRes(){
 	local r=$?
 	eval $resVar="'$resVal'"
 	if [ "$debug" = 1 ]; then
-		(>&2 echo -e Result Value: $cYellow$resVal$cDefault)
+		(>&2 echo -e Result Value: $cYellow$resVal$cNone)
 	fi
 	return $r
 }
@@ -101,39 +109,6 @@ installedPpa() {
  run "find /etc/apt/ -name '*.list' -print0 | xargs -0 grep -ho '^deb http://ppa.launchpad.net/[a-z0-9\\-]\\+/[a-z0-9\\-]\\+'"
 }
 
-#echo "testing:"
-#echo "done"
-#exit
-
-# update & upgrade
-if check_yes_no "update & upgrade?" "n"; then
-	$SUDO apt update
-	$SUDO apt upgrade
-fi
-
-if check_yes_no "turn on sticky keys?" "n"; then
-	xkbsetinstall=0
-	if ! isCmd xkbset; then
-		echo "need xkbset, which is not installed"
-		if check_yes_no "install xkbset?"; then
-			xkbsetinstall=1
-			install "xkbset"
-		fi
-	fi
-	if isCmd xkbset; then
-		echo ""
-		run "xkbset a sticky -twokey -latchlock"
-		run "xkbset exp =sticky"
-		echo "you should add theese somewhere to startup, or turn on in settings..."
-		read
-	elif [ "$xkbsetinstall" = 1 ]; then
-		echo -e $cRed"xkbset install failed!"$cDefault
-	fi
-fi
-
-cd $HOME
-
-
 essentials=(apt add-apt-repository git curl ssh sed)
 missing=()
 for cmd in "${essentials[@]}"; do
@@ -144,13 +119,13 @@ done
 
 if [ ! ${#missing[@]} = 0 ]; then
 	echo "we need theese essential programs for this script:"
-	echo -e $cBlue${missing[@]}$cDefault
+	echo -e $cCmd${missing[@]}$cNone
 	if check_yes_no "install and continue?"; then
 		installed=()
 		for pkg in ${missing[@]}; do
 			install $pkg
 			if ! isCmd $pkg; then
-				echo -e $cRed"$pkg install failed!"$cDefault
+				echo -e $cErr"$pkg install failed!"$cNone
 				#revert installed missing?
 				if [ ! ${#installed[@]} = 0 ]; then
 					echo "reverting installed missing: ${installed[@]}"
@@ -168,6 +143,40 @@ fi
 unset missing
 unset essentials
 
+cd $HOME
+
+#echo "testing:"
+#
+#echo "done"
+#exit
+
+# update & upgrade
+if check_yes_no "update & upgrade?" "n"; then
+	$SUDO apt update
+	$SUDO apt upgrade
+fi
+
+if check_yes_no "turn on sticky keys?" "n"; then
+	xkbsetinstall=0
+	if ! isCmd xkbset; then
+		echo "need ${cCmd}xkbset${cNone}, which is not installed"
+		if check_yes_no "install ${cPkg}xkbset${cNone}?"; then
+			xkbsetinstall=1
+			install "xkbset"
+		fi
+	fi
+	if isCmd xkbset; then
+		echo ""
+		run "xkbset a sticky -twokey -latchlock"
+		run "xkbset exp =sticky"
+		echo "you should add theese somewhere to startup, or turn on in settings..."
+		read
+	elif [ "$xkbsetinstall" = 1 ]; then
+		echo -e $cErr"xkbset install failed!"$cNone
+	fi
+fi
+
+
 githubName="jezek"
 github="https://github.com/$githubName"
 githubSsh=0
@@ -184,7 +193,7 @@ if [ ! "$res" = 1 ]; then
 		if [ -z $pubKeyFile ]; then
 			if check_yes_no "no public key ($sshPubKeyFile) found. create new?"; then
 				if ! isCmd "ssh-keygen"; then
-					if check_yes_no "this operation needs ${cBlue}ssh-keygen${cDefault}. install?"; then
+					if check_yes_no "this operation needs ${cPkg}ssh-keygen${cNone}. install?"; then
 						install "ssh-keygen"
 					fi
 				fi
@@ -198,7 +207,7 @@ if [ ! "$res" = 1 ]; then
 					fi
 					run "ssh-keygen -t rsa -b 4096 $commentAttr"
 				else
-					echo -e $cRed"generating ssh key failed"$cDefault
+					echo -e $cErr"generating ssh key failed"$cNone
 				fi
 				if [ -e $sshPubKeyFile ]; then
 					pubKeyFile=$sshPubKeyFile
@@ -221,17 +230,17 @@ if [ ! "$res" = 1 ]; then
 					if [ "$res" = 1 ]; then
 						githubSsh=1
 					else
-						echo -e $cRed"someting failed, github with ssh access not configured"$cDefault
+						echo -e $cErr"someting failed, github with ssh access not configured"$cNone
 					fi
 				else
-					echo -e $cRed"Can not set key to gitHub as $githubName"$cDefault
+					echo -e $cErr"Can not set key to gitHub as $githubName"$cNone
 				fi
 			else
-				echo -e $cRed"Can not load key from "$cDefault$pubKeyFile
+				echo -e $cErr"Can not load key from "$cNone$pubKeyFile
 			fi
 			unset pubKey
 		else
-			echo -e $cRed"no public key found, github with ssh access not configured"$cDefault
+			echo -e $cErr"no public key found, github with ssh access not configured"$cNone
 		fi
 	fi
 else
@@ -248,27 +257,26 @@ if [ ! -d $dotfilesDir ]; then
 	gitdotfiles=""
 	run "git clone $github/.dotfiles.git"
 	if [ ! -d $dotfilesDir ]; then
-		echo "clonning $dotfilesDir from git failed"
+		echo "clonning ${cFile}$dotfilesDir${cNone} from github failed"
 		exit 1
 	fi
 fi
 
+#TODO git credentials helper?
+#[credential]
+#	helper = /usr/share/doc/git/contrib/credential/gnome-keyring/git-credential-gnome-keyring
 GITFILES="$dotfilesDir/git/files"
 GITCONFIG=".gitconfig"
 if [ -e "$GITFILES/$GITCONFIG" ]; then
-	if check_yes_no "configure $GITCONFIG from $GITFILES/$GITCONFIG?"; then
+	if check_yes_no "configure ${cFile}$GITCONFIG${cNone} from ${cFile}$GITFILES/$GITCONFIG${cNone}?"; then
 		if [ -e $GITCONFIG ]; then
-			echo "$GITCONFIG exists"
-			if check_yes_no "backup $GITCONFIG?"; then
+			echo "${cFile}$GITCONFIG${cNone} exists"
+			if check_yes_no "backup ${cFile}$GITCONFIG${cNone}?"; then
 				GITCONFIGBACKUP="$GITCONFIG.bak"
-				echo "backing up to $GITCONFIGBACKUP"
+				echo "backing up to ${cFile}$GITCONFIGBACKUP${cNone}"
 				run "mv $GITCONFIG $GITCONFIGBACKUP"
 			else
 				run "rm $GITCONFIG"
-			fi
-			if [ -e $GITCONFIG ]; then
-				echo "failed"
-				exit 1
 			fi
 		fi
 		run "cp -vibl $GITFILES/$GITCONFIG $GITCONFIG"
@@ -278,76 +286,107 @@ fi
 PROFILE=".profile"
 DOTPROFILE="$dotfilesDir/shell/profile"
 if [ -e $DOTPROFILE ]; then
-	if check_yes_no "use $DOTPROFILE as $PROFILE?"; then
+	if check_yes_no "use ${cFile}$DOTPROFILE${cNone} as ${cFile}$PROFILE${cNone}?"; then
 		if [ -e $PROFILE ]; then
-			echo "$PROFILE exists"
-			if check_yes_no "backup $PROFILE?"; then
+			echo "${cFile}$PROFILE${cNone} exists"
+			if check_yes_no "backup ${cFile}$PROFILE${cNone}?"; then
 				PROFILEBACKUP="$PROFILE.bak"
-				echo "backing up to $PROFILEBACKUP"
+				echo "backing up to ${cFile}$PROFILEBACKUP${cNone}"
 				run "mv $PROFILE $PROFILEBACKUP"
 			else
 				run "rm $PROFILE"
-			fi
-			if [ -e $PROFILE ]; then
-				echo "failed"
-				exit 1
 			fi
 		fi
 		run "cp -vilb $DOTPROFILE $PROFILE"
 	fi
 fi
 
+bashrc=".bashrc"
+dotBashrcFile="$dotfilesDir/bash/bashrc"
+if [ -e $dotBashrcFile ]; then
+	if check_yes_no "use ${cFile}$dotBashrcFile${cNone} as ${cFile}$bashrc${cNone}?"; then
+		if [ -e $bashrc ]; then
+			echo "$bashrc exists"
+			if check_yes_no "backup ${cFile}$bashrc${cNone}?"; then
+				bashrcBackup="$bashrc.bak"
+				echo "backing up to ${cFile}$bashrcBackup${cNone}"
+				run "mv $bashrc $bashrcBackup"
+			else
+				run "rm $bashrc"
+			fi
+		fi
+		run "cp -vilb $dotBashrcFile $bashrc"
+	fi
+fi
+bashAliases=".bash_aliases"
+dotBashAliasesFile="$dotfilesDir/bash/bash_aliases"
+if [ -e $dotBashAliasesFile ]; then
+	if check_yes_no "use ${cFile}$dotBashAliasesFile${cNone} as ${cFile}$bashAliases${cNone}?"; then
+		if [ -e $bashAliases ]; then
+			echo "$bashAliases exists"
+			if check_yes_no "backup ${cFile}$bashAliases${cNone}?"; then
+				bashAliasesBackup="$bashAliases.bak"
+				echo "backing up to ${cFile}$bashAliasesBackup${cNone}"
+				run "mv $bashAliases $bashAliasesBackup"
+			else
+				run "rm $bashAliases"
+			fi
+		fi
+		run "cp -vilb $dotBashAliasesFile $bashAliases"
+	fi
+fi
+
 if ! isCmd vim; then
-	echo "vim not installed"
-	if check_yes_no "install vim?"; then
+	echo "${cCmd}vim${cNone} not installed"
+	if check_yes_no "install ${cPkg}vim?${cNone}"; then
 		install "vim"
 	fi
 fi
 
 if isCmd vim; then
-	if check_yes_no "configure vim?"; then
+	if check_yes_no "configure ${cCmd}vim${cNone}?"; then
 
 		VIMFILES="$dotfilesDir/vim/files"
 		if [ ! -d $VIMFILES ]; then
-			echo "directory $VIMFILES does not exists"
+			echo "directory ${cDir}$VIMFILES${cNone} does not exists"
 			exit 1
 		fi
 
 		VIMDIR=".vim"
 		if [ -d $VIMDIR ]; then
-			echo "$VIMDIR directory exists"
+			echo "${cDir}$VIMDIR${cNone} directory exists"
 			if check_yes_no "backup $VIMDIR?"; then
 				VIMBACKUP="$VIMDIR.bak"
-				echo "backing up to $VIMBACKUP"
+				echo "backing up to ${cDir}$VIMBACKUP${cNone}"
 				run "mv $VIMDIR $VIMBACKUP"
 			else
 				run "rm -rf $VIMDIR"
 			fi
 			if [ -d $VIMDIR ]; then
 				echo "failed"
-				exit 1
+				read
 			fi
 		fi
 		run "cp -vilbr $VIMFILES/.vim ."
 
 		VIMRC=".vimrc"
 		if [ ! -e "$VIMFILES/$VIMRC" ]; then
-			echo "file $VIMFILES/$VIMRC does not exists"
+			echo "file ${cFile}$VIMFILES/$VIMRC${cNone} does not exists"
 			exit 1
 		fi
 
 		if [ -e $VIMRC ]; then
-			echo "$VIMRC exists"
-			if check_yes_no "backup $VIMRC?"; then
+			echo "${cFile}$VIMRC${cNone} exists"
+			if check_yes_no "backup ${cFile}$VIMRC${cNone}?"; then
 				VIMRCBACKUP="$VIMRC.bak"
-				echo "backing up to $VIMRCBACKUP"
+				echo "backing up to ${cFile}$VIMRCBACKUP${cNone}"
 				run "mv $VIMRC $VIMRCBACKUP"
 			else
 				run "rm $VIMRC"
 			fi
 			if [ -e $VIMRC ]; then
 				echo "failed"
-				exit 1
+				read
 			fi
 		fi
 		run "cp -vilb $VIMFILES/$VIMRC ./$VIMRC"
@@ -364,24 +403,24 @@ if isCmd vim; then
 fi #type vim 2>/dev/null
 
 if ! isCmd gvim; then
-	echo "gvim not installed"
-	if check_yes_no "install vim-gtk3?"; then
+	echo "${cCmd}gvim${cNone} not installed"
+	if check_yes_no "install ${cPkg}vim-gtk3${cNone}?"; then
 		install "vim-gtk3"
 		if ! isCmd gvim; then
 			echo "failed"
-			exit 1
+			read
 		fi
 	fi
 fi
 
 
 if ! isCmd mc; then
-	echo "mc not installed"
-	if check_yes_no "install mc?"; then
+	echo "${cCmd}mc${cNone} not installed"
+	if check_yes_no "install ${cPkg}mc${cNone}?"; then
 		install "mc"
 		if ! isCmd mc; then
 			echo "failed"
-			exit 1
+			read
 		fi
 	fi
 fi
@@ -389,7 +428,7 @@ fi
 if isCmd mc; then
 	MCFILES="$dotfilesDir/mc/files"
 	if [ -d $MCFILES ]; then
-		if check_yes_no "configure mc from $MCFILES?"; then
+		if check_yes_no "configure ${cCmd}mc${cNone} from ${cDir}$MCFILES${cNone}?"; then
 			run "cp -vibr $MCFILES/.config ."
 		fi
 	fi
@@ -397,16 +436,16 @@ fi
 
 
 if ! isCmd audacious; then
-	echo "audacious not found"
-	if check_yes_no "install audacious?"; then
+	echo "${cCmd}audacious${cNone} not found"
+	if check_yes_no "install ${cPkg}audacious audacious-plugins${cNone}?"; then
 		install "audacious audacious-plugins"
 		if ! isCmd audacious; then
 			echo "failed"
-			exit 1
+			read
 		fi
 		if isCmd rhythmbox; then
-			echo "rhythmbox found"
-			if check_yes_no "purge rhythmbox?"; then
+			echo "${cCmd}rhythmbox${cNone} found"
+			if check_yes_no "purge ${cPkg}rhythmbox${cNone}?"; then
 				run "$SUDO apt purge rhythmbox"
 			fi
 			if isCmd rhythmbox; then
@@ -420,30 +459,30 @@ fi
 if isCmd audacious; then
 	AUDACIOUSFILES="$dotfilesDir/audacious/files"
 	if [ -d $AUDACIOUSFILES ]; then
-		if check_yes_no "configure audacious from $AUDACIOUSFILES?"; then
+		if check_yes_no "configure ${cCmd}audacious${cNone} from ${cDir}$AUDACIOUSFILES${cNone}?"; then
 			run "cp -vibr $AUDACIOUSFILES/.config ."
 		fi
 		#TODO mimeapps.list text replace to .config/mimeapps.list
-		echo "associate audacious with audio files (from $AUDACIOUSFILES/mimeapps.list to .config/mimeapps.list"
+		echo "associate audacious with audio files (from ${cFile}$AUDACIOUSFILES/mimeapps.list${cNone} to ${cFile}.config/mimeapps.list${cNone}"
 		read
 	fi
 fi
 
 
 if ! isCmd chromium-browser; then
-	echo "chromium not found"
-	if check_yes_no "install chromium?"; then
+	echo "${cCmd}chromium${cNone} not found"
+	if check_yes_no "install ${cPkg}chromium-browser${cNone}?"; then
 		install "chromium-browser"
 		if ! type chromium-browser 2>/dev/null; then
 			echo "failed"
-			exit 1
+			read
 		fi
 	fi
 fi
 
 if isCmd firefox; then
-	echo "firefox found"
-	if check_yes_no "purge firefox?"; then
+	echo "${cCmd}firefox${cNone} found"
+	if check_yes_no "purge ${cPkg}firefox${cNone}?"; then
 		run "$SUDO apt purge firefox"
 	fi
 	if isCmd firefox; then
@@ -472,10 +511,10 @@ if [ $res = 0 ]; then
 		unset supported
 
 	else
-		echo -e $cRed"can not fetch usb devices"$cDefault
+		echo -e $cErr"can not fetch usb devices"$cNone
 	fi
 else
-	echo -e $cRed"can not fetch supported fprint devices"$cDefault
+	echo -e $cErr"can not fetch supported fprint devices"$cNone
 fi
 
 if [ ! -z ${fingerprintSupported+x} ]; then
@@ -500,16 +539,16 @@ if [ ! -z ${fingerprintSupported+x} ]; then
 		  run $SUDO" apt install libbsapi policykit-1-fingerprint-gui fingerprint-gui"
 			if isCmd "fingerprint-gui"; then
 				echo "fingerprint support installed"
-				echo -e "to configure fingerprint run ${cGreen}fingerprint-gui${cDefault}"
-				echo -e $cRed"GNOME and KDE users WARNING:"$cDefault
+				echo -e "to configure fingerprint run ${cCmd}fingerprint-gui${cNone}"
+				echo -e $cWarn"GNOME and KDE users WARNING:"$cNone
 				echo "if uninstalling, see Uninstall section at: $fingerprintPpaUrl"
 				echo ""
 
-				if check_yes_no "run ${cGreen}fingerprint-gui${cDefault}?"; then
+				if check_yes_no "run ${cCmd}fingerprint-gui${cNone}?"; then
 					run "fingerprint-gui"
 				fi
 			else
-				echo -e $cRed"fingerprint not installed"$cDefault
+				echo -e $cErr"fingerprint not installed"$cNone
 			fi
 		fi
 		unset ppaName
