a2e3dc2c089dea49d4405b600cd60d0b11e6981a
jEzEk <jezek@chicki.sk>
2018-09-25 17:57:07 +0200

    add description to todo

--- installers/ssh/install.sh
+++ installers/ssh/install.sh
@@ -1,4 +1,4 @@
 exit
-TODO
+#TODO ip detection & try ssh to all ips (find out which are my devices) & mount/remount all devices
 
-IPs=$(sudo arp-scan --localnet --numeric --quiet --ignoredups | grep -E '([a-f0-9]{2}:){5}[a-f0-9]{2}' | awk '{print $1}')
+IPs=sudo arp-scan --localnet --numeric --quiet --ignoredups | grep -E '([a-f0-9]{2}:){5}[a-f0-9]{2}' | awk '{print $1}'
